c54912012aa6916e89af8619bd55d8968cc14746
瞿龙俊 <154256698@qq.com>
2018-02-07 19:24:12 +0800

    推送订餐

--- pages/core/dc/dc_confirm.js
+++ pages/core/dc/dc_confirm.js
@@ -15,6 +15,93 @@ Page({
     },
     showError: false
   },
+  formSubmit: function (e) {
+    var form_id = e.detail.formId;
+    var _this = this,
+      formData = _this.data.formData;
+      formData.formId = form_id;
+    _this.setData({
+      showError: true
+    });
+    // 验证表单
+    if (!formData.id || !formData.time || !formData.foodId) {
+      return false;
+    }
+
+
+    function getDate() {
+      var nowDate = new Date();
+      var year = nowDate.getFullYear();
+      var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
+        : nowDate.getMonth() + 1;
+      var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate
+        .getDate();
+      var dateStr = year + "-" + month + "-" + day;
+      return dateStr;
+    }
+
+    function isLate() {
+      var standard = getDate() + " 16:00:00";
+      var standardDate = new Date(standard);
+      if (standardDate < (new Date())) {
+        //只能明天起
+        return false;
+      } else {
+        //可以今天
+        return true;
+      }
+    }
+    if (!isLate() && (formData.time).split(" ")[0]==getDate()){
+      app.showErrorModal("目前已经无法预订当日晚餐", "订餐失败");
+      return false;
+    }
+
+    wx.showModal({
+      title: '提示',
+      content: '是否预定' + formData.time + '的晚餐' + formData.food + '？',
+      success: function (res) {
+        if (res.confirm) {
+          formData.userId = app._user.we.id;
+          wx.request({
+            url: app._server + '/api/order/create',
+            method: 'POST',
+            data: formData,
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: function (res) {
+              if (res.data && res.data.code === 200) {
+                wx.showToast({
+                  title: '订餐成功',
+                  icon: 'success',
+                  duration: 2000,
+                  complete: function () {
+                    setTimeout(function () {
+                      wx.navigateBack({
+                        delta: 2
+                      });
+                    }, 2000);
+                  }
+                });
+                // wx.navigateBack();
+              } else {
+                var errorMessage = (res.data.data && res.data.data.reason) || res.data.message;
+                if (res.data && res.data.code === 502) {
+                  errorMessage = "您已预定过当日晚餐，更改菜品请先取消订单！";
+                }
+
+                app.showErrorModal(errorMessage, "订餐失败");
+              }
+            },
+            fail: function (res) {
+              app.showErrorModal(res.errMsg);
+            }
+          });
+        }
+      }
+    });
+
+  },
   onLoad: function(option){
     console.log(option);
     if(!app._user.we.id || !app._user.we.realName){
